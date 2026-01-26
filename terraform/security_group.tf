226e6d7fc0f921ee22f4fadf4d84252f1b0ca091
Goodness Mogbonjubola <your-github-email@example.com>
2026-01-26 05:38:48 -0800

    New Configurations

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -2,6 +2,16 @@ resource "aws_security_group" "web" {
   name   = "${var.project_name}-sg"
   vpc_id = aws_vpc.main.id
 
+ # Add or update the SSH ingress rule
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow from anywhere
+  }
+
+
   # Allow HTTP traffic from anywhere
   ingress {
     from_port   = 80
